9fb410be2f3a262918b9e9897896b5f3b57b2d8f
Mfurbee77 <mchristian0428@gmail.com>
2021-06-28 21:24:25 -0500

    Assignment 3 Complete :)

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,23 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//I added 3 parameters for the model and search parameters.  I also displayed an array list of jobs.
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+         // If the input is blank or they type all, then all jobs are displayed.
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+         // Otherwise pass the search parameters to the find by column and value method
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+       //Without the column choices as attributes, the view will not display.
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 
 }
